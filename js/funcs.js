5beabb247ed3c2feb3d3d1fc821200ad79a56a7a
Gleb Averchuk <altermn@gmail.com>
2012-10-25 13:27:18 +0400

    new markup inner pages

--- js/funcs.js
+++ js/funcs.js
@@ -15,8 +15,29 @@ $(document).ready(function()
     }).mouseout(function(){
             $(this).find('ul').hide();
         });
+    $(".hidden-ul").mouseover(function(){
+        $(this).find('ul').show();
+    }).mouseout(function(){
+            $(this).find('ul').hide();
+        });
+    $(".search-box div").mouseover(function(){
+        $(this).addClass('hover');
+    }).mouseout(function(){
+            $(this).removeClass('hover');
+        });
     var ww = $('.slides_container').width();
     $('.slide').width(ww);
+    var wrap = $(window).width();
+    if (wrap<1065) { $('body').addClass('small-nav'); }
+    else { $('body').removeClass('small-nav'); }
+    var wpp = $('.popup-form').width();
+    var hrap = $(window).height();
+    var hpp = $('.popup-form').height();
+    var ktop, kleft;
+    ktop = (hrap-hpp)/2;
+    kleft = (wrap-wpp)/2;
+    $('.popup-form').css({top: ktop+'px', left: kleft+'px'});
+
     $(window).resize(function(){
         var mmn;
         $('.bottom-block-cont section').css({height: 'auto'});
@@ -28,16 +49,29 @@ $(document).ready(function()
         $('.slide').width(ww);
         var hsl =  $('.slide').height();
         $('.slides_control').height(hsl);
+        var wrap = $(window).width();
+        if (wrap<1065) { $('body').addClass('small-nav'); }
+        else { $('body').removeClass('small-nav'); }
+        var wpp = $('.popup-form').width();
+        var hrap = $(window).height();
+        var hpp = $('.popup-form').height();
+        var ktop, kleft;
+        ktop = (hrap-hpp)/2;
+        kleft = (wrap-wpp)/2;
+        $('.popup-form').css({top: ktop+'px', left: kleft+'px'});
     });
 });
 $(window).load(function(){
-    var mmn;
+    var mmn, leftX, likoor;
     mmn = $('.bottom-block-cont').data('mmn');
     var hh = $('.bottom-block-cont').height();
     hh=hh-mmn;
     $('.bottom-block section').height(hh);
     var hsl =  $('.slide').height();
     $('.slides_control').height(hsl);
+    likoor=$('.hidden-ul a').position().left;
+    leftX=$('.hide-ul').data('left');
+    $('.hide-ul').css({left: (likoor-leftX)+'px'});
 });
 $(function(){
     $('#slides').slides({
